023705cc91958c951e7537d408f2e22ac580eb3e
Isak Sylvin <isak.sylvin@scilifelab.se>
2017-03-03 10:13:49 +0100

    Added projectname to known attributes

--- taca_ngi_pipeline/deliver/deliver.py
+++ taca_ngi_pipeline/deliver/deliver.py
@@ -87,6 +87,14 @@ class Deliverer(object):
         self.reportpath = getattr(self, 'reportpath', None)
         self.force = getattr(self, 'force', False)
         self.stage_only = getattr(self, 'stage_only', False)
+        try:
+            getattr(self, 'projectname')
+        except AttributeError:
+            try:
+                n = db.project_entry(db.dbcon(), projectid)['name']
+                self.projectname = n
+            except KeyError:
+                pass
         # only set an attribute for uppnexid if it's actually given or in the db
         try:
             getattr(self, 'uppnexid')
